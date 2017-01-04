c82ed83db2f62d3c2b0b068df7edf864bc463351
Mahmoud Swehli <m_swehli@hotmail.com>
2017-01-04 19:28:59 +0000

    template updates transfered over

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass')
+var sass = require('gulp-sass');
+var stripCssComments = require('gulp-strip-css-comments');
+var removeEmptyLines = require('gulp-remove-empty-lines');
+var minify = require('gulp-minify');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
 
 var browserSync = require('browser-sync').create();
 
@@ -10,11 +16,13 @@ gulp.task('default', function()
     browserSync.init({
         server: {
             baseDir: ""
-        }
+        },
+        cors:true
     });
     
      gulp.watch('sass/**/*.scss',['sass']);
      gulp.watch('**/*.html').on('change', browserSync.reload);
+     //gulp.watch('Scripts/*.js',['minify']);
 });
 
 
@@ -22,6 +30,9 @@ gulp.task('sass', function()
 {
     return gulp.src('sass/main.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(stripCssComments())
+    .pipe(removeEmptyLines())
+    .pipe(cleanCSS())
     .pipe(gulp.dest('CSS'))
     .pipe(browserSync.stream()); 
 });
@@ -32,4 +43,26 @@ gulp.task('browser-sync', function() {
             baseDir: ""
         }
     });
+});
+
+gulp.task('minify',function()
+{
+    return gulp.src('scripts/*.js')
+    .pipe(minify({
+        ext:{
+            src:'.js',
+            min:'.min.js'
+        },
+        ignoreFiles:['.min.js'],
+        noSource: true
+    }))
+    .pipe(concat('site.js'))
+    .pipe(gulp.dest('Scripts/dist'));
+});
+
+gulp.task('imagemin',function()
+{
+    return gulp.src('media/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('media/dist'));
 });
