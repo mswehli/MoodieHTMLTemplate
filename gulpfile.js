33b09172873b107ad1baf002beb298a2086880e5
Mahmoud Swehli <m_swehli@hotmail.com>
2017-07-18 16:18:59 +0200

    updates to moodio, added updated compenents and more vars

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,15 @@
+/*
+ * Place    <!-- build:css --> <!-- endbuild -->
+ * around CSS link tags, to replace with cleaned/minified css
+ * and <!-- build:js --> <!-- endbuild --> around js tags.
+ * e.g.:
+ * <!-- build:js -->
+ * <script src="scripts/slider.js" async></script>
+ * <script src="scripts/hammer.min.js" async></script>
+ * <!-- endbuild -->
+ * Will replace both script tags with
+ * <script src="scripts/site.min.js" async></script>
+ */
 'use strict';
 
 var gulp = require('gulp');
@@ -8,6 +20,8 @@ var minify = require('gulp-minify');
 var cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
 var imagemin = require('gulp-imagemin');
+var htmlreplace = require('gulp-html-replace');
+var htmlmin = require('gulp-htmlmin');
 
 var browserSync = require('browser-sync').create();
 
@@ -65,4 +79,53 @@ gulp.task('imagemin',function()
     return gulp.src('media/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('media/dist'));
+});
+
+
+/*dockerbuild tasks*/
+gulp.task('dockerbuild',['docker-minify','docker-sass','docker-imagemin','docker-copy']);
+
+gulp.task('docker-minify',function()
+{
+    return gulp.src('scripts/*.js')
+    .pipe(minify({
+        ext:{
+            src:'.js',
+            min:'.min.js'
+        },
+        ignoreFiles:['.min.js'],
+        noSource: true
+    }))
+    .pipe(concat('site.min.js'))
+    .pipe(gulp.dest('dist/scripts'));
+})
+
+gulp.task('docker-sass', function()
+{
+    return gulp.src('sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(stripCssComments())
+    .pipe(removeEmptyLines())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream()); 
+});
+
+gulp.task('docker-imagemin',function()
+{
+    return gulp.src('media/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/media'));
+});
+
+gulp.task('docker-copy',function()
+{
+    gulp.src('*.html')
+    .pipe(htmlreplace({
+        'css': 'css/main.css',
+        'js': {src:'scripts/site.min.js', tpl: '<script src="%s" async></script>'}
+    }))
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist/'));
+
 });
